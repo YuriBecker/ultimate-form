1f51750eb6648d06bbf94017cf0a0ffd49d253f1
Yuri Becker <becker.yr@gmail.com>
2022-04-20 16:49:14 -0300

    fix: payment schema validation

--- utils/schemas/payment/schema.js
+++ utils/schemas/payment/schema.js
@@ -7,7 +7,7 @@ const paymentSchema = validator.object().shape({
     .string()
     .required()
     .matches(/^(0[1-9]|1[0-2])\/([0-9]{4}|[0-9]{2})$/, "Formato inválido"),
-  securityCode: validator
+  cvc: validator
     .string()
     .required()
     .min(3, "Deve ter no mínimo 3 dígitos")
